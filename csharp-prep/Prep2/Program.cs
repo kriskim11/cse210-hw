527a7a8955538bbe9c1322a6ef6b8c1ad283b6a9
kriskim11 <131200162+kriskim11@users.noreply.github.com>
2024-03-02 00:25:12 -0800

    added csharp prep 1-5

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,45 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+      static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine ("what is your grade percentage?");
+        string answer = Console.ReadLine();
+        float percent = float.Parse(answer);
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >=80)
+        {
+            letter = "B";
+        }
+        else if (percent >=70)
+        {
+            letter = "C";
+        }
+        else if(percent >=60)
+        {
+            letter ="D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Your Grade is {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("you passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("better luck next time");
+        }
+
     }
+
 }
