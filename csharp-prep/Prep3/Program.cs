0fac62a67170576d2411e70ae9ee3f69e2c77c71
kriskim11 <131200162+kriskim11@users.noreply.github.com>
2024-03-02 00:17:56 -0800

    added csharp prep 1-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumberGenerator = new Random();
+        int magicNumber = randomNumberGenerator.Next(1,100);
+        int guess = -1;
+        int attempts =0;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("I have selected a number betweeen 1 and 100");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+    
+            
+            if (magicNumber>guess){
+                Console.WriteLine("higher");
+
+            }
+            else if (magicNumber < guess){
+                Console.WriteLine("lower");
+            }
+            else 
+            {
+                Console.WriteLine($"Congratulations!you guessed the number {magicNumber} in {attempts} attempts");
+                break;
+            }
+        }
+        
+
+
     }
 }
